eefacb36fa77b025831ddf156bce6f7bf9e3c754
Danny Lee <dreamer@panix.com>
2019-10-06 22:04:27 -0400

    Done.

--- run.rb
+++ run.rb
@@ -1,3 +1,3 @@
-require_relative "lib/Model1.rb"
-require_relative "lib/Model2.rb"
-require_relative "lib/Model3.rb"
+require_relative "lib/Appointment.rb"
+require_relative "lib/Doctor.rb"
+require_relative "lib/Patient.rb"
